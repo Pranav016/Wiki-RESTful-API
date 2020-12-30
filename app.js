8d69f6792bf2fc7abe0e6f6c4d5308d108b59ff6
Pranav <pranavmendi@gmail.com>
2020-12-31 04:26:19 +0530

    Connected to the DB added get request for the API

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static("public"));
 
+//Connecting to Mongo database using ODM Mongoose-
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const wikiSchema = {
+    title: String,
+    content: String,
+};
+
+const Article = new mongoose.model("Article", wikiSchema);
+
+app.get("/articles", function(req, res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        }
+        else{
+            res.send(err);
+        }
+    });
+});
+
 //Launching the server on port 3000-
 app.listen(3000, function(){
     console.log("Server started on port 3000");
